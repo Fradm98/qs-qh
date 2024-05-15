cf52e7fe7432e20f10d3071587b9bcd67d690f6d
Fradm98 <fradm@kth.se>
2024-05-16 01:54:30 +0200

    improving the execution automization. Need of nested circuit layout

--- src/qs_qh/execute_circuits.py
+++ src/qs_qh/execute_circuits.py
@@ -35,7 +35,7 @@ def construct_logical_circuit(NUM_QUBITS: int, DEPTH: int, type_circ: QuantumCir
     return logical_circuit
 
 def construct_logical_Pauli_observable(NUM_QUBITS: int, pauli_weight: int, type_pauli: str):
-    paulis = ['I'*i + 'Z'*pauli_weight + 'I'*(NUM_QUBITS-i-pauli_weight) for i in range(NUM_QUBITS)]
+    paulis = ['I'*i + type_pauli*pauli_weight + 'I'*(NUM_QUBITS-i-pauli_weight) for i in range(NUM_QUBITS)]
     coeffs = 1/len(paulis)
     logical_observable = SparsePauliOp(paulis, coeffs)
     return logical_observable
@@ -56,32 +56,12 @@ def estimator_options(shots: int=4096, err_suppr: int=0, err_mitig: int=0, zne: 
     options.optimization_level = err_suppr  # Deactivate error suppression
     options.resilience_level = err_mitig  # Deactivate error mitigation
     options.resilience.zne_mitigation = zne  # Activate ZNE error mitigation only
+    if err_mitig == 0:
+        options.resilience.measure_mitigation = False
+    else:
+        options.resilience.measure_mitigation = True
     return options
 
-def construct_batches_execution(LIST_QUBITS: list, LIST_DEPTHS: list, stat_shots: int=100, shots: int=4096, err_suppr: int=0, err_mitig: int=0, zne: bool=True, pauli_weight: int=1, type_pauli: str="Z"):    
-    backend = least_busy_backend()
-    options = estimator_options(shots=shots, err_suppr=err_suppr, err_mitig=err_mitig, zne=zne)
-
-    jobs_batches = []
-    for NUM_QUBITS in LIST_QUBITS: 
-        for DEPTH in LIST_DEPTHS:
-            print(f"construct logical circuit...")
-            logical_circuit = construct_logical_circuit(NUM_QUBITS=NUM_QUBITS, DEPTH=DEPTH, type_circ="mbl", assign_params="fixed", dagger=True)
-            print(f"construct logical observable...") 
-            logical_observable = construct_logical_Pauli_observable(NUM_QUBITS=NUM_QUBITS, pauli_weight=pauli_weight, type_pauli=type_pauli)
-            print(f"construct physical circuit...")
-            physical_circuit = construct_physical_circuit(logical_circuit=logical_circuit, backend=backend)
-            print(f"construct physical observable...")
-            physical_observable = construct_physical_Pauli_observable(physical_circuit=physical_circuit, logical_observable=logical_observable)
-
-            for shot in range(stat_shots):
-                print(f"n_qubits: {NUM_QUBITS}, depth: {DEPTH}, stat: {shot}")
-                jb = construct_batch_execution(physical_circuit=physical_circuit, physical_observable=physical_observable, backend=backend, options=options, stat_shot=shot, zne=zne)            
-                jobs_batch = {"n_qubits": NUM_QUBITS, "depth": DEPTH, "batch": jb}
-                jobs_batches.append(jobs_batch)
-    save_jobs(filename=f"logs/jobs_qubits_{LIST_QUBITS}_depths_{LIST_DEPTHS}_pauli-{type_pauli}_weight_{pauli_weight}_stat_shots_{stat_shots}_shots_{shots}_err_suppr_{err_suppr}_err_mitig_{err_mitig}_zne_{zne}", jobs=jobs_batches)
-
-    
 def construct_batch_execution(physical_circuit: QuantumCircuit, physical_observable: SparsePauliOp, backend: ibm_backend, options: EstimatorOptions, stat_shot: int, zne: bool):    
     jobs_batch = {}
     with Batch(backend=backend) as batch:
@@ -103,15 +83,43 @@ def construct_batch_execution(physical_circuit: QuantumCircuit, physical_observa
             extrapolator = 'exponential'
             estimator.options.resilience.zne.extrapolator = extrapolator
             jobs_batch[extrapolator] = estimator.run([pub])
+
         else:
             # no zne
             extrapolator = 'no_zne'
             jobs_batch[extrapolator] = estimator.run([pub])
-                
+
+
     jb = []
     for extrapolator, job_batch in jobs_batch.items():
-        dict_batch = {"job_stat_number":stat_shot, "job_id":job_batch.job_id(), "extrapolator": extrapolator}
+        dict_batch = {"job_stat_number":stat_shot, "job_id":job_batch.job_id(), "extrapolator": extrapolator, "mem": options.resilience.measure_mitigation}
         jb.append(dict_batch)
     return jb
 
+def construct_batches_execution(LIST_QUBITS: list, LIST_DEPTHS: list, backend=None, stat_shots: int=100, shots: int=4096, err_suppr: int=0, err_mitig: int=0, zne: bool=True, pauli_weight: int=1, type_pauli: str="Z"):    
+    if backend == None:
+        backend = least_busy_backend()
+    options = estimator_options(shots=shots, err_suppr=err_suppr, err_mitig=err_mitig, zne=zne)
 
+    jobs_batches = []
+    for NUM_QUBITS in LIST_QUBITS: 
+        for DEPTH in LIST_DEPTHS:
+            print(f"construct logical circuit...")
+            logical_circuit = construct_logical_circuit(NUM_QUBITS=NUM_QUBITS, DEPTH=DEPTH, type_circ="mbl", assign_params="fixed", dagger=True)
+            print(f"construct logical observable...") 
+            logical_observable = construct_logical_Pauli_observable(NUM_QUBITS=NUM_QUBITS, pauli_weight=pauli_weight, type_pauli=type_pauli)
+            print(f"construct physical circuit...")
+            physical_circuit = construct_physical_circuit(logical_circuit=logical_circuit, backend=backend)
+            print(f"construct physical observable...")
+            physical_observable = construct_physical_Pauli_observable(physical_circuit=physical_circuit, logical_observable=logical_observable)
+
+            for shot in range(stat_shots):
+                print(f"n_qubits: {NUM_QUBITS}, depth: {DEPTH}, stat: {shot}")
+                jb = construct_batch_execution(physical_circuit=physical_circuit, physical_observable=physical_observable, backend=backend, options=options, stat_shot=shot, zne=zne)            
+                jobs_batch = {"n_qubits": NUM_QUBITS, "depth": DEPTH, "batch": jb}
+                jobs_batches.append(jobs_batch)
+    if err_mitig == 0:
+        mem = False
+    else:
+        mem = True
+    save_jobs(filename=f"logs/jobs_qubits_{LIST_QUBITS}_depths_{LIST_DEPTHS}_pauli-{type_pauli}_weight_{pauli_weight}_stat_shots_{stat_shots}_zne_{zne}_mem_{mem}", jobs=jobs_batches)
--- src/qs_qh/utils.py
+++ src/qs_qh/utils.py
@@ -3,11 +3,11 @@ import json
 
 def call_ibm_account():
     try:
-        service = QiskitRuntimeService()
+        service = QiskitRuntimeService(name="ibm-ikerbasque")
     except:
-        print("Use the token from IBM Industry session and save the account")
-        service = QiskitRuntimeService(channel="ibm_quantum", token="4463e0fe7fb2e7d70ba9dfe7eebe7ffc7ef8bf2c57cd2c74f40af32e8b063534f46e1e794ee505523a7d5ca10b20a742b0f0b2306a8959f200d0154c91a36a6c")  # Credentials may be needed
-        service.save_account(channel="ibm_quantum", token="4463e0fe7fb2e7d70ba9dfe7eebe7ffc7ef8bf2c57cd2c74f40af32e8b063534f46e1e794ee505523a7d5ca10b20a742b0f0b2306a8959f200d0154c91a36a6c", name="ibm_industry_session", overwrite=True)
+        print("Use the token from IBM quantum ikerbasque collab and save the account")
+        service = QiskitRuntimeService(channel="ibm_quantum", token="0fb1cd48a37e2722ff37089427a3d8616d7b2287a908d94dfb62520a17e05294adc3458b0ddcd2333774eb5497b16f75965bbb0233cf9c3b0df7343ffa1374b9")  # Credentials may be needed
+        service.save_account(channel="ibm_quantum", token="0fb1cd48a37e2722ff37089427a3d8616d7b2287a908d94dfb62520a17e05294adc3458b0ddcd2333774eb5497b16f75965bbb0233cf9c3b0df7343ffa1374b9", name="ibm-ikerbasque", overwrite=True)
         print(f"{[key for key, value in service.saved_accounts().items()]} account saved")
     return service
 
@@ -15,7 +15,7 @@ def least_busy_backend():
     service = call_ibm_account()
     print(service.backends(simulator=False))
     lb = service.least_busy(simulator=False)
-    print(lb)
+    print(lb.name)
     backend = service.get_backend(name=lb.name)
     return backend
 
@@ -23,12 +23,13 @@ def save_jobs(filename: str, jobs: dict):
     with open(f'{filename}.json', 'w') as json_file:
         json.dump(jobs, json_file, indent=4)
 
-def retrieve_job_id(n_qubits, depth, extrapolation_method):
-    with open('jobs.json', 'r') as json_file:
+def retrieve_job_id(n_qubits, depth, extrapolation_method, mem, filename):
+    ids = []
+    with open(f'{filename}.json', 'r') as json_file:
         jobs = json.load(json_file)
         for job in jobs:
-            print(job)
             if job['n_qubits'] == n_qubits and job['depth'] == depth:
                 for c_dict in job['batch']:
-                    if c_dict['extrapolator'] == extrapolation_method:
-                        return c_dict['job_id']
+                    if c_dict['extrapolator'] == extrapolation_method and c_dict['mem'] == mem:
+                        ids.append(c_dict['job_id'])
+    return ids
