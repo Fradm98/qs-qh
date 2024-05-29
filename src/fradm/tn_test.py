bcaee41b4a90a9d3f57fbfb640b3ea7f201c6426
Fradm98 <fradm@kth.se>
2024-05-29 17:19:37 +0200

    adding correct dmrg and observable for the massive z2 chain. working on tebd

--- src/fradm/tn_test.py
+++ src/fradm/tn_test.py
@@ -18,7 +18,7 @@ class Z2_chain_massive_mps(MPS):
     def mpo(self, long="Z", trans="X"):
         if self.model == "Z2_chain_massive":
             self.Z2_chain_massive_mpo()
-
+    
     def Z2_chain_massive_mpo(self):
         I = identity(self.d, dtype=complex).toarray()
         O = csc_array((self.d, self.d), dtype=complex).toarray()
@@ -42,11 +42,57 @@ class Z2_chain_massive_mps(MPS):
         self.w = w_tot
         return self
     
-chain = Z2_chain_massive_mps(L=61, d=2, chi=64, h1=0.5, h2=0.1)
-chain._random_state(seed=3, chi=16)
-chain.canonical_form()
-chain.DMRG(trunc_tol=False, trunc_chi=True)
-# chain.DMRG(trunc_tol=True, trunc_chi=False)
-tensor_shapes(chain.sites)
+    def local_order_param(self, site: int, long: str="Z"):
+        X = sparse_pauli_x(n=0,L=1).toarray()
+        Z = sparse_pauli_z(n=0,L=1).toarray()
 
-# chain.mpo_first_moment()
+        if long == "Z":
+            op = Z
+        elif long == "X":
+            op = X
+
+        self.Z2.mpo_skeleton(aux_dim=2)
+        mpo = self.Z2.mpo
+        mpo_tot = []
+        for i in range(self.L):
+            if i == (site):
+                mpo[0,-1] = op
+            mpo_tot.append(mpo)
+            self.Z2.mpo_skeleton(aux_dim=2)
+            mpo = self.Z2.mpo
+        self.w = mpo_tot
+        return self
+    
+    def TEBD_Z2_chain(self, params_quench):
+
+        # initialize ancilla with a state
+        self._compute_norm(site=1)
+        self.ancilla_sites = self.sites.copy()
+
+        errors = [[0, 0]]
+        entropies = [0
+                     ]
+        trotter_steps = params_quench.get('trotter_steps')
+        delta = params_quench.get('delta')
+        h_1 = params_quench.get('h_1')
+        h_2 = params_quench.get('h_2')
+        n_sweeps = params_quench('n_sweeps')
+        conv_tol = params_quench('conv_tol')
+        bond = params_quench('bond')
+        where = params_quench('where')
+        for trott in range(trotter_steps):
+            print(f"------ Trotter steps: {trott} -------")
+            self.mpo_quench(delta, h_1, h_2)
+            print(f"Bond dim ancilla: {self.ancilla_sites[self.L//2].shape[0]}")
+            print(f"Bond dim site: {self.sites[self.L//2].shape[0]}")
+            error, entropy = self.compression(
+                trunc_tol=False,
+                trunc_chi=True,
+                n_sweeps=n_sweeps,
+                conv_tol=conv_tol,
+                bond=bond,
+                where=where,
+            )
+            self.ancilla_sites = self.sites.copy()
+            errors.append(error)
+            entropies.append(entropy)
