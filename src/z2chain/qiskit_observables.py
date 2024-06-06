c3d1b5acaf46310727bc23100d63bd8ca8497a34
Jesús Cobos Jiménez <jesus.victoria5@gmail.com>
2024-06-06 11:50:26 +0200

    Changed file name

--- src/z2chain/qiskit_observables.py
+++ src/z2chain/qiskit_observables.py
@@ -1,5 +1,4 @@
 from qiskit.quantum_info import SparsePauliOp
 
-def local_pauli_z(chain_lenght, qubit_ind):
-    nqubits = 2*chain_lenght - 1
+def local_pauli_z(nqubits, qubit_ind):
     return SparsePauliOp("I"*qubit_ind + "Z" + "I"*(nqubits - qubit_ind - 1))
