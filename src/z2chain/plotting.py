7de4d2b5b113b2c2f03266f827b4b6eb5bb710b8
Jesús Cobos Jiménez <jesus.victoria5@gmail.com>
2024-06-13 10:17:05 +0200

    Plotting functions now indistinctively accept jobs or expectation value arrays

--- src/z2chain/plotting.py
+++ src/z2chain/plotting.py
@@ -1,24 +1,33 @@
+from qiskit.primitives.primitive_job import PrimitiveJob
 import matplotlib.pyplot as plt
 import numpy as np
 
-def x_t_plot(site_guge_observable_matrix):
+def convert_jobs_to_site_gauge_matrix(jobs_arr):
+    site_gauge_observable_matrix = np.zeros((len(jobs_arr), len(jobs_arr[0].result()[0].data.evs)))
+    for i, job in enumerate(jobs_arr):
+        site_gauge_observable_matrix[i] = (1 - job.result()[0].data.evs)/2
+    return site_gauge_observable_matrix
+
+def x_t_plot(site_gauge_observable_matrix):
+    if type(site_gauge_observable_matrix[0]) is PrimitiveJob:
+        site_gauge_observable_matrix = convert_jobs_to_site_gauge_matrix(site_gauge_observable_matrix)
+
     plt.rc("text", usetex=True)
     plt.rc("font", size=24, family="serif", weight="bold")
 
     fig, ax = plt.subplots(1, 1, figsize=[12, 8])
 
-    plt.imshow(site_guge_observable_matrix, cmap="inferno", aspect=site_guge_observable_matrix.shape[0]/site_guge_observable_matrix.shape[1]/15)
+    plt.imshow(site_gauge_observable_matrix, cmap="inferno", aspect=site_gauge_observable_matrix.shape[0]/site_gauge_observable_matrix.shape[1]/15)
     plt.xlabel(r"Sites")
     plt.ylabel("t")
     plt.tight_layout()
 
-def x_t_plot_jobs(jobs_arr):
-    site_gauge_observable_matrix = np.zeros((len(jobs_arr), len(jobs_arr[0].result()[0].data.evs)))
-    for i, job in enumerate(jobs_arr):
-        site_gauge_observable_matrix[i] = (1 - job.result()[0].data.evs)/2
-    x_t_plot(site_gauge_observable_matrix)
-
 def discrepancies_plot(exact_site_gauge_observable_matrix, approximated_site_gauge_observable_matrix):
+    if type(exact_site_gauge_observable_matrix[0]) is PrimitiveJob:
+        exact_site_gauge_observable_matrix = convert_jobs_to_site_gauge_matrix(exact_site_gauge_observable_matrix)
+    if type(approximated_site_gauge_observable_matrix[0]) is PrimitiveJob:
+        approximated_site_gauge_observable_matrix = convert_jobs_to_site_gauge_matrix(approximated_site_gauge_observable_matrix)
+
     plt.rc("text", usetex=True)
     plt.rc("font", size=24, family="serif", weight="bold")
 
@@ -31,9 +40,3 @@ def discrepancies_plot(exact_site_gauge_observable_matrix, approximated_site_gau
     plt.xlabel(r"Sites")
     plt.ylabel("t")
     plt.tight_layout()
-
-def discrepancies_plot_jobs(exact_site_gauge_observable_matrix, jobs_arr):
-    approximated_site_gauge_observable_matrix = np.zeros((len(jobs_arr), len(jobs_arr[0].result()[0].data.evs)))
-    for i, job in enumerate(jobs_arr):
-        approximated_site_gauge_observable_matrix[i] = (1 - job.result()[0].data.evs)/2
-    discrepancies_plot(exact_site_gauge_observable_matrix, approximated_site_gauge_observable_matrix)
