8d96ce55d7162ebc17fcc86c5511943c415fb48a
Jesús Cobos Jiménez <jesus.victoria5@gmail.com>
2024-07-22 18:20:39 +0200

    Added local pauli x operators

--- src/z2chain/operators.py
+++ src/z2chain/operators.py
@@ -2,3 +2,6 @@ from qiskit.quantum_info import SparsePauliOp
 
 def local_pauli_z(nqubits, qubit_ind):
     return SparsePauliOp("I"*qubit_ind + "Z" + "I"*(nqubits - qubit_ind - 1))
+
+def local_pauli_x(nqubits, qubit_ind):
+    return SparsePauliOp("I"*qubit_ind + "X" + "I"*(nqubits - qubit_ind - 1))
