d4b4cda9c2d62e327e5e1ee1dc975f24521f96d2
Jesús Cobos Jiménez <jesus.victoria5@gmail.com>
2024-07-22 12:07:13 +0200

    Added option for X basis + removed old longer circuits

--- src/z2chain/circs.py
+++ src/z2chain/circs.py
@@ -4,53 +4,47 @@ from qiskit.transpiler import PassManager, StagedPassManager, Layout
 from qiskit.circuit import QuantumCircuit, Parameter
 from qiskit.converters import circuit_to_instruction
 from utils.circs import remove_idle_qwires
-from qiskit import QuantumRegister
 import numpy as np
 
-class LocalInteractionPropagator(QuantumCircuit):
-    def __init__(self):
-        super().__init__(3)
-        t = Parameter("t")
-        self.cx(1, 0)
-        self.cx(1, 2)
-        self.rx(2*t, 1)
-        self.cx(1, 2)
-        self.cx(1, 0)
-
-class LowDepthLocalInteractionPropagatorFirst(QuantumCircuit):
-    def __init__(self):
+class LocalInteractionPropagatorFirst(QuantumCircuit):
+    def __init__(self, x_basis=False):
         super().__init__(2)
-        self.cx(1, 0)
+        if x_basis:
+            self.cx(0, 1)
+        else:
+            self.cx(1, 0)
         
-class LowDepthLocalInteractionPropagatorSecond(QuantumCircuit):
-    def __init__(self):
+class LocalInteractionPropagatorSecond(QuantumCircuit):
+    def __init__(self, x_basis=False):
         super().__init__(2)
-        self.cx(0, 1)
+        if x_basis:
+            self.cx(1, 0)
+        else:
+            self.cx(0, 1)
+
+class TotalSingleBodyPropagator(QuantumCircuit):
+    def __init__(self, chain_length, x_basis=False):
+        nqubits = 2*chain_length - 1
+        super().__init__(nqubits)
+        t_tau = Parameter("t_τ")
+        t_sigma = Parameter("t_σ")
+        if x_basis:
+            self.rx(2*t_tau, np.arange(0, self.num_qubits, 2))
+            self.rx(2*t_sigma, np.arange(1, self.num_qubits, 2))
+        else:
+            self.rz(2*t_tau, np.arange(0, self.num_qubits, 2))
+            self.rz(2*t_sigma, np.arange(1, self.num_qubits, 2))
 
 class TotalInteractionPropagator(QuantumCircuit):
-    def __init__(self, chain_length):
-        nqubits = 2*chain_length - 1
-        qubit_list = np.arange(nqubits)
-        int_qb_inds_first = qubit_list[np.less(qubit_list % 4, 3)][:-1 if chain_length % 2 else None]
-        int_qb_inds_second = qubit_list[2::][np.less((qubit_list[2::] - 2) % 4, 3)][:None if chain_length % 2 else -1]
-        super().__init__(nqubits)
-        local_prop = LocalInteractionPropagator()
-        local_prop_instruction = circuit_to_instruction(local_prop)
-        for qubits in int_qb_inds_first.reshape((len(int_qb_inds_first)//3, 3)):
-            self.append(local_prop_instruction, list(qubits))
-        for qubits in int_qb_inds_second.reshape((len(int_qb_inds_second)//3, 3)):
-            self.append(local_prop_instruction, list(qubits))
-
-class LowDepthTotalInteractionPropagator(QuantumCircuit):
-    def __init__(self, chain_length):
+    def __init__(self, chain_length, x_basis=False):
         nqubits = 2*chain_length - 1
         qubit_list = np.arange(nqubits)
         int_qb_inds_first = np.array(qubit_list[:-1])
         int_qb_inds_second = np.array(qubit_list[1:])
         t = Parameter("t")
         super().__init__(nqubits)
-        local_prop1 = LowDepthLocalInteractionPropagatorFirst()
-        local_prop2 = LowDepthLocalInteractionPropagatorSecond()
+        local_prop1 = LocalInteractionPropagatorFirst(x_basis)
+        local_prop2 = LocalInteractionPropagatorSecond(x_basis)
         local_prop_instruction1 = circuit_to_instruction(local_prop1)
         local_prop_instruction2 = circuit_to_instruction(local_prop2)
         for qubits in int_qb_inds_first.reshape((len(int_qb_inds_first)//2, 2)):
@@ -59,9 +53,10 @@ class LowDepthTotalInteractionPropagator(QuantumCircuit):
         for qubits in int_qb_inds_second.reshape((len(int_qb_inds_second)//2, 2)):
             self.append(local_prop_instruction2, list(qubits))
 
-        for qubits in range(nqubits):
-            if qubits%2 == 1:
-                self.rx(2*t, qubits)
+        if x_basis:
+            self.rz(2*t, np.arange(1, nqubits, 2))
+        else:
+            self.rx(2*t, np.arange(1, nqubits, 2))
 
         for qubits in int_qb_inds_second.reshape((len(int_qb_inds_second)//2, 2)):
             self.append(local_prop_instruction2, list(qubits))
@@ -69,20 +64,21 @@ class LowDepthTotalInteractionPropagator(QuantumCircuit):
         for qubits in int_qb_inds_first.reshape((len(int_qb_inds_first)//2, 2)):
             self.append(local_prop_instruction1, list(qubits))
 
-class TotalSingleBodyPropagator(QuantumCircuit):
-    def __init__(self, chain_length):
+class particle_pair_initial_state(QuantumCircuit):
+    def __init__(self, chain_length, left_particle_position, particle_pair_length=1, x_basis=False):
         nqubits = 2*chain_length - 1
         super().__init__(nqubits)
-        t_tau = Parameter("t_τ")
-        t_sigma = Parameter("t_σ")
-        self.rz(2*t_tau, np.arange(0, self.num_qubits, 2))
-        self.rz(2*t_sigma, np.arange(1, self.num_qubits, 2))
+        if x_basis:
+            self.x(range(nqubits))
+            self.z(range(2*left_particle_position, 2*(left_particle_position + particle_pair_length) + 1))
+        else:
+            self.x(range(2*left_particle_position, 2*(left_particle_position + particle_pair_length) + 1))
 
-def FirstOrderTrotter(chain_length, J, h, lamb, t_total, layers, sqrot_first=False, barriers=False):
+def FirstOrderTrotter(chain_length, J, h, lamb, t_total, layers, sqrot_first=False, x_basis=False, barriers=False):
     t_layer = t_total/layers
-    total_interaction_propagator = TotalInteractionPropagator(chain_length).decompose()
+    total_interaction_propagator = TotalInteractionPropagator(chain_length, x_basis).decompose()
     total_interaction_propagator.assign_parameters([lamb*t_layer], inplace=True)
-    total_single_body_propagator = TotalSingleBodyPropagator(chain_length)
+    total_single_body_propagator = TotalSingleBodyPropagator(chain_length, x_basis)
     total_single_body_propagator.assign_parameters([h*t_layer, t_layer*J], inplace=True)
     if sqrot_first:
         layer = total_single_body_propagator.compose(total_interaction_propagator)
@@ -92,48 +88,29 @@ def FirstOrderTrotter(chain_length, J, h, lamb, t_total, layers, sqrot_first=Fal
         layer.barrier()
     return layer.repeat(layers).decompose()
 
-def SecondOrderTrotter(chain_length, J, h, lamb, t_total, layers, barriers=False):
+def SecondOrderTrotter(chain_length, J, h, lamb, t_total, layers, x_basis=False, barriers=False):
     t_layer = t_total/layers
-    total_interaction_propagator = TotalInteractionPropagator(chain_length).decompose()
+    total_interaction_propagator = TotalInteractionPropagator(chain_length, x_basis).decompose()
     total_interaction_propagator.assign_parameters([lamb*t_layer], inplace=True)
-    total_single_body_propagator = TotalSingleBodyPropagator(chain_length)
+    total_single_body_propagator = TotalSingleBodyPropagator(chain_length, x_basis)
     total_single_body_propagator.assign_parameters([h*t_layer/2, t_layer*J/2], inplace=True)
     layer = total_single_body_propagator.compose(total_interaction_propagator).compose(total_single_body_propagator)
     if barriers: layer.barrier()
     return layer.repeat(layers).decompose()
 
-def LowDepthSecondOrderTrotter(chain_length, J, h, lamb, t_total, layers, barriers=False):
-    t_layer = t_total/layers
-    total_interaction_propagator = LowDepthTotalInteractionPropagator(chain_length).decompose()
-    total_interaction_propagator.assign_parameters([lamb*t_layer], inplace=True)
-    total_single_body_propagator = TotalSingleBodyPropagator(chain_length)
-    total_single_body_propagator.assign_parameters([h*t_layer/2, t_layer*J/2], inplace=True)
-    layer = total_single_body_propagator.compose(total_interaction_propagator).compose(total_single_body_propagator)
-    if barriers: layer.barrier()
-    return layer.repeat(layers).decompose()
-
-class particle_pair_initial_state(QuantumCircuit):
-    def __init__(self, chain_length, left_particle_position, particle_pair_length=1):
-        nqubits = 2*chain_length - 1
-        super().__init__(nqubits)
-        self.x(range(2*left_particle_position, 2*(left_particle_position + particle_pair_length) + 1))
-
-def particle_pair_quench_simulation_circuits(chain_length, J, h, lamb, particle_pair_left_position, particle_pair_length, final_time, layers, measure_every_layers=1, barriers=False):
-    initial_state_preparation = particle_pair_initial_state(chain_length, particle_pair_left_position, particle_pair_length)
+def particle_pair_quench_simulation_circuits(chain_length, J, h, lamb, particle_pair_left_position, particle_pair_length, final_time, layers, measure_every_layers=1, x_basis=False, barriers=False):
+    initial_state_preparation = particle_pair_initial_state(chain_length, particle_pair_left_position, particle_pair_length, x_basis=x_basis)
     circs_to_return = [initial_state_preparation]
     ncircuits_to_iterate = layers // measure_every_layers
     for i in range(1, ncircuits_to_iterate + 1):
-        this_trotter_circuit = SecondOrderTrotter(chain_length, J, h, lamb, final_time*i/ncircuits_to_iterate, i*measure_every_layers, barriers=barriers)
+        this_trotter_circuit = SecondOrderTrotter(chain_length, J, h, lamb, final_time*i/ncircuits_to_iterate, i*measure_every_layers, x_basis=x_basis, barriers=barriers)
         this_complete_circuit = initial_state_preparation.compose(this_trotter_circuit)
         circs_to_return.append(this_complete_circuit)
     return circs_to_return
 
-def physical_particle_pair_quench_simulation_circuits(chain_length, J, h, lamb, particle_pair_left_position, particle_pair_length, final_time, layers, backend, optimization_level, layout=None, measure_every_layers=1, depth="low", barriers=False):
-    initial_state_preparation_circ = particle_pair_initial_state(chain_length, particle_pair_left_position, particle_pair_length)
-    if depth == "low":
-        logical_trotter_layer_circ = LowDepthSecondOrderTrotter(chain_length, J, h, lamb, final_time/layers, 1, barriers)
-    else:
-        logical_trotter_layer_circ = SecondOrderTrotter(chain_length, J, h, lamb, final_time/layers, 1, barriers)
+def physical_particle_pair_quench_simulation_circuits(chain_length, J, h, lamb, particle_pair_left_position, particle_pair_length, final_time, layers, backend, optimization_level, layout=None, measure_every_layers=1, x_basis=False, barriers=False):
+    initial_state_preparation_circ = particle_pair_initial_state(chain_length, particle_pair_left_position, particle_pair_length, x_basis=x_basis)
+    logical_trotter_layer_circ = SecondOrderTrotter(chain_length, J, h, lamb, final_time/layers, 1, x_basis=x_basis, barriers=barriers)
     layout = layout[:logical_trotter_layer_circ.num_qubits] if layout is not None else None
     pm = generate_preset_pass_manager(optimization_level=optimization_level, backend=backend, initial_layout=layout)
     physical_state_preparation_circuit = pm.run(initial_state_preparation_circ)
